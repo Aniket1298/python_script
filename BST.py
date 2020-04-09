bee90b945b94108100e9a30d4cd3e0c2f2292348
Aniket1298 <tekina1298@gmail.com>
2020-04-09 21:52:10 +0530

    BST with duplicate numbers

--- BST.py
+++ BST.py
@@ -1,8 +1,9 @@
 class Node:
-	def __init__(self,val):
+	def __init__(self,val,f=1):
 		self.val=val        #Value of Node
 		self.left=None      #Left Child
 		self.right=None     #Right Child
+		self.f=f			#frequency of the number 
 def insert(node,root):
 	if node.val<root.val:
 		if root.left==None:
@@ -14,14 +15,28 @@ def insert(node,root):
 			root.right=node
 		else:
 			insert(node,root.right)
-def inorder(root):
+	else:
+		root.f+=1
+def search(n,root):
+	if root==None:
+		print n,"is not present in the BST"
+	elif n<root.val:
+		search(n,root.left)
+	elif n>root.val:
+		search(n,root.right)
+	else:
+		print  n,"is present in the BST"
+def inorder(root):    #Inorder traversal 
 	if root==None:
 		return
 	inorder(root.left)
-	print root.val
+	for i in range(root.f):
+		print root.val,
 	inorder(root.right)
 Root=Node(3)  #Defining ROot Node
-l=[2,4,1,6,9,-1,0,5]
+l=[2,4,1,6,9,-1,0,5,5,4,1,1,1,1,4]
 for i in l:
 	insert(Node(i),Root)
-inorder(Root)
+print "Inorder Traversal:",inorder(Root)
+print 
+search(7,Root)
